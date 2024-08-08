d877898bf37a5adcb8440cc21b34c6be9b4d4f01
guido <guidofacio91@gmail.com>
2024-08-08 14:08:26 -0300

    3 commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { MongoClient, ObjectId } = require('mongodb');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs'); // Cambiado a bcryptjs
 
 const app = express();
 const port = 5000;
